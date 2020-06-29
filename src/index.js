3a9fe7a6dd182209fde7100c059ecee59077839b
ChayaGreisman <chayamgreisman@gmail.com>
2020-06-28 21:38:47 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+document.addEventListener("DOMContentLoaded", e => {
+  const clickHandler = () => {
+    const button = document.querySelector("button")
+    button.addEventListener("click", e => {
+      getUser()
+    })
+  }
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
-});
+  const getUser = () => {
+    fetch("https://randomuser.me/api/")
+    .then(response => response.json())
+    .then(data => {
+      const user = data.results[0]
+      renderUser(user)
+    })
+  }
+  
+  function renderUser(user){
+
+    const fullname = `${user.name.title} ${user.name.first} ${user.name.last}`
+    document.querySelector('#fullname').textContent = fullname
+
+    const email = user.email
+    document.querySelector('#email').textContent = email
+
+    const profile_picture = user.picture.medium
+    document.querySelector('#profile_picture').src = profile_picture
+
+    const street = `${user.location.street.number} ${user.location.street.name}`
+    document.querySelector('#street').textContent = street
+  
+  }
+  
+  clickHandler()
+})
